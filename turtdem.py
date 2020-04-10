d2a1011120dfccc077143951a7c120e79e28c8c7
Stanislaw Jaromin <jaromin@onet.pl>
2020-04-10 20:55:16 +0200

    Next update

--- turtdem.py
+++ turtdem.py
@@ -1,10 +1,8 @@
-import turtle
+from turtle import forward, right
 import time
 
-# noinspection PyUnresolvedReferences
-
-turtle.forward(150)
-turtle.right(250)
-turtle.forward(150)
+forward(150)
+right(250)
+forward(150)
 
 time.sleep(6)
